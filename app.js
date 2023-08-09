d9424da581bf7365d6205806a38a7dd0b79bf787
Kuldeep Sen <kuldeepsen134@gmail.com>
2023-08-09 16:28:44 +0530

    Allow all origins, methods, and headers

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(cors({
-    origin: '*',
+    origin: ['https://ai-astoria-staging.netlify.app', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: '*',
 }));
